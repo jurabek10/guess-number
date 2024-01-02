0c261df30e3c45cd61f786dbb7201742591177b1
jurabek10 <samiev.jurabek@bk.ru>
2024-01-03 00:39:47 +0900

    Add some DOM manipulation

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct answer ';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 3;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
